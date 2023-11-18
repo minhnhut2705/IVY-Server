afd7ae7f2b726797d6db5962dae010d1f623091e
minhnhut2705 <nhutnguyen486@gmail.com>
2023-11-18 11:26:04 +0700

    remove process env

--- app/config/index.js
+++ app/config/index.js
@@ -3,7 +3,7 @@ const config = {
         port: 3000,
     },
     db: {
-        uri: process.env.MONGODB_URI || "mongodb+srv://nhutb1910428:Minhnhut27052001@cluster0.goa5uls.mongodb.net/Few?retryWrites=true&w=majority"
+        uri: "mongodb+srv://nhutb1910428:Minhnhut27052001@cluster0.goa5uls.mongodb.net/Few?retryWrites=true&w=majority"
     }
 };
 module.exports = config;
